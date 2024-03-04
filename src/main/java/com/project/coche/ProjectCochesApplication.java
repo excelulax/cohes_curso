1cc0527db05ff3fc207f8ce1248f405c005ca0f0
Charles <celta.loxcel@gmail.com>
2024-03-04 15:10:56 -0400

    "start project"

---
+++ src/main/java/com/project/coche/ProjectCochesApplication.java
@@ -0,0 +1,13 @@
+package com.project.coche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCochesApplication.class, args);
+	}
+
+}
